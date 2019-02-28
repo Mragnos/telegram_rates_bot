4c787bc83228a2c0d0a78d3fd6bda545e8e84204
Mragnos <progrrkbl@gmail.com>
2019-03-01 02:41:06 +0500

    Webhook

--- rates_bot.py
+++ rates_bot.py
@@ -106,13 +106,19 @@ def handle_currency(message):
 
 
 server = Flask(__name__)
+
+
 @server.route("/bot", methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
+
+
 @server.route("/")
 def webhook():
-     bot.remove_webhook()
-     bot.set_webhook(url="https://ratesbot.herokuapp.com/bot") # этот url нужно заменить на url вашего Хероку приложения
-     return "?", 200
+    bot.remove_webhook()
+    bot.set_webhook(url="https://ratesbot.herokuapp.com/bot") # этот url нужно заменить на url вашего Хероку приложения
+    return "?", 200
+
+
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
