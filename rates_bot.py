22bf528a767adecd23136b4c23886af3302d778c
Mragnos <progrrkbl@gmail.com>
2019-03-01 00:28:00 +0500

    Webhook

--- rates_bot.py
+++ rates_bot.py
@@ -113,29 +113,21 @@ def handle_message(message):
     bot.send_message(chat_id=message.chat.id, text='Узнай курс валют', reply_markup=keyboard)
 
 
-
-
 if "HEROKU" in list(os.environ.keys()):
     logger = telebot.logger
     telebot.logger.setLevel(logging.INFO)
 
     server = Flask(__name__)
-
     @server.route("/bot", methods=['POST'])
     def getMessage():
         bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
         return "!", 200
-
     @server.route("/")
     def webhook():
         bot.remove_webhook()
-        bot.set_webhook(url="https://ratesbot.herokuapp.com/")
+        bot.set_webhook(url="https://ratesbot.herokuapp.com")
         return "?", 200
-    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+    server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
 else:
     bot.remove_webhook()
     bot.polling(none_stop=True)
-
-
-#if __name__ == '__main__':
-   # bot.polling(none_stop=True)
